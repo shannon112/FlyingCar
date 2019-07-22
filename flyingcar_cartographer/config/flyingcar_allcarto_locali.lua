0d16748cb1647d6992b40e3a43e3f100dc644fb3
shannon112 <b03505011@ntu.edu.tw>
2019-07-22 16:06:15 +0800

    fixed cartographer localization bugs(1.0.0 and master differ)

--- flyingcar_cartographer/config/flyingcar_allcarto_locali.lua
+++ flyingcar_cartographer/config/flyingcar_allcarto_locali.lua
@@ -14,9 +14,12 @@
 
 include "flyingcar_allcarto.lua"
 
-TRAJECTORY_BUILDER.pure_localization_trimmer = {
-  max_submaps_to_keep = 3,
-}
+-- TRAJECTORY_BUILDER.pure_localization_trimmer = {
+--   max_submaps_to_keep = 3,
+-- }
+
+
+TRAJECTORY_BUILDER.pure_localization = true
 POSE_GRAPH.optimize_every_n_nodes = 20
 
 return options
